c67078b46f7618f96d02f0b1a702f6b592dc662e
seojeongmoon <55705552+seojeongmoon@users.noreply.github.com>
2019-10-28 15:37:21 +0100

    Add "Put" Actuator Data to turn on/off LED

--- saul_coap.c
+++ saul_coap.c
@@ -32,6 +32,7 @@ static ssize_t _saul_dev_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void
 static const coap_resource_t _resources[] = {
     { "/saul/cnt", COAP_GET, _saul_cnt_handler, NULL },
     { "/saul/dev", COAP_POST, _saul_dev_handler, NULL },
+	{"/saul/atr", COAP_PUT, _saul_atr_handler, NULL}
 };
 
 static gcoap_listener_t _listener = {
@@ -146,4 +147,63 @@ static ssize_t _saul_cnt_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void
 void saul_coap_init(void)
 {
     gcoap_register_listener(&_listener);
+}
+
+static ssize_t _saul_atr_handler(coap_pkt_t* pdu, uint8_t *buf, size_t len, void *ctx)
+{
+    uint8_t type;
+
+    (void)ctx;
+
+    if (pdu->payload_len <= 5) {
+        char req_payl[6] = { 0 };
+        memcpy(req_payl, (char *)pdu->payload, pdu->payload_len);
+        type = atoi(req_payl);
+    }
+    else {
+        return gcoap_response(pdu, buf, len, COAP_CODE_BAD_REQUEST);
+    }
+	
+    return _atr_type_responder(pdu, buf, len, type);
+}
+
+static ssize_t _atr_type_responder(coap_pkt_t* pdu, uint8_t *buf, size_t len, uint8_t type)
+{
+    saul_reg_t *dev = saul_reg_find_type(type);
+    phydat_t res;
+	phydat_dump(phydat_t *res, type);//phydat.h
+    int dim;
+    size_t resp_len;
+    gcoap_resp_init(pdu, buf, len, COAP_CODE_CONTENT);
+    coap_opt_add_format(pdu, COAP_FORMAT_TEXT);
+    resp_len = coap_opt_finish(pdu, COAP_OPT_FINISH_PAYLOAD);
+	
+    if (dev == NULL) {
+	char *err = "device not found";
+	if (pdu->payload_len >= strlen(err)) {
+	    memcpy(pdu->payload, err, strlen(err));
+	    resp_len += gcoap_response(pdu, buf, len, COAP_CODE_404);
+	    return resp_len;
+	}
+	else {
+	    return gcoap_response(pdu, buf, len, COAP_CODE_404);
+	}
+    }
+
+    dim = saul_reg_write(dev, &res); // &res -as saul_reg_read store location or *res data to write? 
+    if (dim <= 0) {
+	char *err = "no values found";
+	if (pdu->payload_len >= strlen(err)) {
+	    memcpy(pdu->payload, err, strlen(err));
+	    resp_len += gcoap_response(pdu, buf, len, COAP_CODE_404);
+	    return resp_len;
+	}
+	else {
+	    return gcoap_response(pdu, buf, len, COAP_CODE_404);
+	}
+    }
+
+    /* write the response buffer with the request device value */
+    resp_len += fmt_u16_dec((char *)pdu->payload, res.val[0]);
+    return resp_len;
 }
